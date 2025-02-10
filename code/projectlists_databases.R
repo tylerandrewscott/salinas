7add67748bcbd317cfd02941f9fa535a8b26dace
Sara Ludwick <saraludwick@campus-012-226.ucdavis.edu>
2025-02-10 14:27:48 -0800

    file organizing

--- code/projectlists_databases.R
+++ code/projectlists_databases.R
@@ -2,15 +2,14 @@
 url <- "https://github.com/tylerandrewscott/salinas/raw/refs/heads/main/solarwind_project_details.RDS"
 projects_all <- readRDS(url(url, method = "libcurl"))
 projects <- projects_all[,c("EIS.Title", "Document.Type", "State", "Federal.Register.Date")]
-# add empty columns to fill in
-projects$Project.Name <- NA
-projects$Database <- NA
 # get wind or solar project type
 projects$Type <- ifelse(grepl("solar", projects$EIS.Title, ignore.case = T), "Solar", "other")
 projects$Type <- ifelse(grepl("wind", projects$EIS.Title, ignore.case = T), "Wind", projects$Type)
+# add empty columns to fill in with info from solar/wind databases
+projects$Project.Name <- NA
+projects$Database <- NA
 # sort alphabetically, by state, and by type
 projects <- projects[order(projects$Type, projects$State, projects$EIS.Title),]
-# save
-# dir <- "/Users/saraludwick/Library/CloudStorage/Box-Box/projects/sara_salinas/"
-# write.csv(projects, paste0(dir,"project_boundaries/projectlist.csv"))
+# save csv to box
+write.csv(projects,"salinasbox/intermediate_data/project_databases/projectlist.csv")
 
