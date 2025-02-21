1eae6e25d21ce5779e8da651d2dfaed4a59d80a7
Sara Ludwick <saraludwick@campus-031-234.ucdavis.edu>
2025-02-21 15:42:37 -0800

    code to get polygons for wind projects from turbine point locations

--- code/spatialdata_processing.R
+++ code/spatialdata_processing.R
@@ -28,16 +28,51 @@ gis_list_transformed <- lapply(gis_list, function(x) {
     }
   })
 
+# ex <- st_transform(ex, st_crs(solar_projs))
+# ex2 <- st_transform(ex2, st_crs(solar_projs))
+
 library(dplyr)
 #turn wind turbine points into polygons
 ex <- wind_projs[1:80,]
 ex2 <- ex %>%
   group_by(p_name, p_year) %>% # get turbines from same year at same project
   summarise(geometry = st_union(geometry)) %>% # merge into multipoint 
-  ungroup() %>% 
-  mutate(geometry = st_convex_hull(geometry)) 
-# decide what to do with single points or lines that result if no poly is made. add buffers to those?
+  mutate(geometry = st_convex_hull(geometry)) %>%  
+  rowwise() %>% # add buffers to points and lines that result if didn't make polygon
+  mutate(geometry = if (st_geometry_type(geometry) != "POLYGON") {
+    st_buffer(geometry, dist = 1)
+  } else {
+    geometry
+  }) %>%
+  ungroup()
+
+# what the polygons look like 
+plot(st_geometry(ex2[ex2$p_name == "25 Mile Creek",], max.plot = 1))
+plot(st_geometry(ex[ex$p_name == "25 Mile Creek",]), col = 'black', add = TRUE)
+
+# what a linestrings look like 
+plot(st_geometry(ex2[ex2$p_name == "6th Space Warning Squadron",], max.plot = 1))
+plot(st_geometry(ex[ex$p_name == "6th Space Warning Squadron",]), col = 'black', add = T)
 
 
-         
-        
+
+
+
+if (n)
+  
+  
+#   ))
+#   if (st_geometry_type(ex2[i,]) != "POLYGON") 
+#     mutate(geometry = st_buffer(ex2[i,], dist = 1))
+#   else mutate(geometry = geometry)
+# }
+# 
+# if (st_geometry_type())
+#   case_when(st_geometry_type(geometry) != "POLYGON" ~ mutate(geometry = st_buffer(geometry, dist = 10), geometry = geometry))
+
+
+
+
+
+
+
