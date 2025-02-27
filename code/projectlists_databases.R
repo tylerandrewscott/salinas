6dbb1e18f670014caaaeec6af376a1e5580d7971
Sara Ludwick <saraludwick@campus-018-252.ucdavis.edu>
2025-02-26 16:35:22 -0800

    prepare spreadsheets for matching to spatial data

--- code/projectlists_databases.R
+++ code/projectlists_databases.R
@@ -1,15 +1,39 @@
-# solar and wind projects from EIS salinas folder
-url <- "https://github.com/tylerandrewscott/salinas/raw/refs/heads/main/solarwind_project_details.RDS"
-projects_all <- readRDS(url(url, method = "libcurl"))
-projects <- projects_all[,c("EIS.Title", "Document.Type", "State", "Federal.Register.Date")]
+# solar and wind project metadata 
+projects_all <- readRDS("salinasbox/solarwind_project_details.RDS")
+projects <- projects_all[,c("EIS.Number", "EIS.Title", "Document.Type", "State", "Federal.Register.Date")] 
 # get wind or solar project type
-projects$Type <- ifelse(grepl("solar", projects$EIS.Title, ignore.case = T), "Solar", "other")
-projects$Type <- ifelse(grepl("wind", projects$EIS.Title, ignore.case = T), "Wind", projects$Type)
-# add empty columns to fill in with info from solar/wind databases
-projects$Project.Name <- NA
-projects$Database <- NA
+projects$Project.Type <- ifelse(grepl("solar", projects$EIS.Title, ignore.case = T), "Solar", "other")
+projects$Project.Type <- ifelse(grepl("wind", projects$EIS.Title, ignore.case = T), "Wind", projects$Project.Type)
+# get rid of any dupes
+projects <- projects[!duplicated(projects),]
 # sort alphabetically, by state, and by type
-projects <- projects[order(projects$Type, projects$State, projects$EIS.Title),]
-# save csv to box
-write.csv(projects,"salinasbox/intermediate_data/project_databases/projectlist.csv")
+projects <- projects[order(projects$Project.Type, projects$State, projects$EIS.Number, projects$EIS.Title),]
+## now filter for ones we will use in our analysis
+eis_pdfs <- list.files("salinasbox/intermediate_data/appendix_removal/done")
+# add 20140004 because it will be added later 
+eis_pdfs <- c(eis_pdfs, "20140004_Fowler_Ridge")
+# get just EIS numbers 
+eis_pdfs_nums <- unique(substr(eis_pdfs, 1, 8))
+# matches
+projects_done <- projects[projects$EIS.Number %in% eis_pdfs_nums,]
+# where are these EIS docs??? -- pre-2012, except for Rail Tie ???
+not_in <- projects[!projects$EIS.Number %in% eis_pdfs_nums,]
+# csv for EIS docs with appendices removed 
+write.csv(projects_done,"salinasbox/intermediate_data/project_databases/EISlist.csv", row.names = F)
+
+# consolidate wind turbine database by unique name and year because these will be combined into polygons 
+wind_projs <- read.csv("salinasbox/intermediate_data/project_databases/processing/wind_projects_smaller.csv")
+wind_projs <- wind_projs[!duplicated(wind_projs[c("p_name", "p_year")]),]
+
+wind_projs <- wind_projs[c("case_id", "t_state", "t_county", "p_name", "p_year")]
+# order by state, proj name, etc. to group potentially same projects together
+wind_projs <- wind_projs[order(wind_projs$t_state, wind_projs$t_county, wind_projs$p_name, wind_projs$p_year),]
+write.csv(wind_projs, "salinasbox/intermediate_data/project_databases/USWTDB.csv", row.names = F)
+
+# order solar to group potentially same projects together
+solar_projs <- read.csv("salinasbox/intermediate_data/project_databases/processing/pv_projects_smaller.csv")
+solar_projs <- solar_projs[,-c(6:7)]
+solar_projs <- solar_projs[order(solar_projs$p_state, solar_projs$p_county, solar_projs$p_name, solar_projs$p_year),]
+write.csv(solar_projs, "salinasbox/intermediate_data/project_databases/USPVDB.csv", row.names = F)
+
 
