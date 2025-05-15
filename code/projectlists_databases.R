9dc92cff8068c0372ea8b073813c0c7a6a296b90
Sara Ludwick <saraludwick@saraludwick.local>
2025-05-15 13:49:03 -0700

    consolidated wind turbines

--- code/projectlists_databases.R
+++ code/projectlists_databases.R
@@ -4,8 +4,6 @@ projects <- projects_all[,c("EIS.Number", "EIS.Title", "Document.Type", "State",
 # get wind or solar project type
 projects$Project.Type <- ifelse(grepl("solar", projects$EIS.Title, ignore.case = T), "Solar", "other")
 projects$Project.Type <- ifelse(grepl("wind", projects$EIS.Title, ignore.case = T), "Wind", projects$Project.Type)
-# get rid of any dupes
-projects <- projects[!duplicated(projects),]
 # sort by type, state, name to bring related projects together
 projects <- projects[order(projects$Project.Type, projects$State, projects$EIS.Title, projects$EIS.Number),]
 # now filter for ones we will use in our analysis
@@ -14,10 +12,15 @@ eis_pdfs <- list.files("salinasbox/intermediate_data/appendix_removal/done")
 eis_pdfs_nums <- unique(substr(eis_pdfs, 1, 8))
 # matches
 projects_done <- projects[projects$EIS.Number %in% eis_pdfs_nums,]
-write.csv(projects_done, "salinasbox/intermediate_data/project_databases/EISlist.csv")
+projects_done <- projects_done[!duplicated(projects_done),]
+
+EIS_file <- "salinasbox/intermediate_data/project_databases/EISlist.csv"
+write.csv(projects_done, EIS_file, row.names = FALSE)
 
 # consolidate wind turbine database by unique name and year because these will be combined into polygons 
+# manually removed some columns when first downloading data, in "processing" folder
 wind_projs <- read.csv("salinasbox/intermediate_data/project_databases/processing/wind_projects_smaller.csv")
+# remove dupes to get single case IDs to make matching easier
 wind_projs <- wind_projs[!duplicated(wind_projs[c("p_name", "p_year")]),]
 wind_projs <- wind_projs[c("case_id", "t_state", "t_county", "p_name", "p_year")]
 # order by state, proj name, etc. to group potentially same projects together
