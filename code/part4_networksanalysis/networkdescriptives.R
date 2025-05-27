13943b44dec58010ba5cd83917eebac1321adb07
Sara Ludwick <saraludwick@campus-105-151.ucdavis.edu>
2025-05-27 11:00:50 -0700

    matching eis network to group

--- code/part4_networksanalysis/networkdescriptives.R
+++ code/part4_networksanalysis/networkdescriptives.R
@@ -5,9 +5,30 @@ filtered_networks <- readRDS("salinasbox/clean_data/filtered_networks.RDS")
 # need to associate projects together, use elise's groups csv
 groups <- read.csv("salinasbox/clean_data/GroupIDs.csv")
 
+
+
+for (i in seq_along(filtered_networks)) {
+  eis <- names(filtered_networks)[i]
+  eis_num <- sub("EIS_", "", eis) # get rid of "EIS_" so just number to match on
+  matches <- match(eis_num, groups$EIS.Number) # get index of matches
+  if(!is.na(matches)) {
+    attr(filtered_networks[[i]], "group") <- groups$Group.Name[matches] # make attribute for group name
+  }
+}
+# what's with those that don't match?
+
+
+
+
+
 vertex_attr(filtered_networks[[2]])
 # why is not resolving "state" and "colorado" for ex a problem? how does this not then like split connections and affect the calculation of network stats?
 # remember that GPE is tricky because this could be referenincing Fort Collins as a city or it could be referencing it geographically right?
+
+summary(filtered_networks)
+
+
+
 
 library(ggraph)
 
