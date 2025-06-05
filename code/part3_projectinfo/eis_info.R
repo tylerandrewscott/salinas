28656a95a32ef845865e2b0bff05abf9af0eb984
Sara Ludwick <saraludwick@campus-037-227.ucdavis.edu>
2025-06-04 19:11:06 -0700

    making table of document descriptive stats

--- code/part3_projectinfo/eis_info.R
+++ code/part3_projectinfo/eis_info.R
@@ -28,10 +28,65 @@ write.csv(projects, EIS_file, row.names = FALSE)
 ################################## variable descriptives ###########################
 
 EIS_info$year <- as.character(EIS_info$year) # to join different classes
-EIS_descriptives <- EIS_info %>%
+EIS_descriptives_all <- EIS_info %>%
   select(c(doc_type, lead_agency, state, year, project_type)) %>%
   pivot_longer(everything(),
                names_to = "variable", values_to = "value") %>%
   group_by(variable, value) %>%
   summarise(n = n()) %>%
-  mutate(p = n/sum(n))
+  mutate(p = n/sum(n)) %>%
+  ungroup() 
+
+# can just use gtsummary to make table
+library(gtsummary)
+library(gt)
+
+table <- tbl_summary(
+  projects,
+  include = c(doc_type, lead_agency, state, year),
+  by = project_type,
+  label = list(doc_type = "Document Type", 
+               lead_agency = "Lead Agency",
+               state = "State",
+               year = "Year")) %>%
+  add_overall() %>%
+  modify_header(label ~ "**Document Attribute**") %>%
+  modify_spanning_header(c("stat_1", "stat_2") ~ "Renewable Project Type")
+
+table
+gtsave(as_gt(table), "salinasbox/presentations_and_outputs/doc_sample_table.html")
+
+########## plots showing document distributions
+library(ggplot2)
+
+# single plots per attribute
+attributes <- unique(EIS_descriptives$variable)
+plots <- lapply(attributes, function(attr) {
+  attribute <- EIS_descriptives %>%
+    filter(variable == paste0(attr))
+  plot <- ggplot(attribute, aes(x = value, y = n)) +
+    geom_col(fill = "antiquewhite4") +
+    labs(x = NULL, y = "Count", title = paste0("number of plans by ", attr)) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))  
+  return(plot)
+})
+
+# plots colored by project type
+
+
+  ggplot(projects, aes(x = year, fill = project_type)) +
+    geom_bar()+
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+
+
+ggplot(EIS_descriptives, aes(x = value, y = n)) +
+  geom_bar(stat = "identity", fill = "antiquewhite4") +
+  facet_wrap(~ variable, scales = "free_x") +
+  labs(x = NULL, y = "Count", title = "distribution of EIS attributes") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
