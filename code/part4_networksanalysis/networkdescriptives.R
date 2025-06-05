42ca6d87dcca1ffaf7a0b512d4f474b66b0ef639
Sara Ludwick <saraludwick@campus-037-227.ucdavis.edu>
2025-06-04 19:15:14 -0700

    add network attributes

--- code/part4_networksanalysis/networkdescriptives.R
+++ code/part4_networksanalysis/networkdescriptives.R
@@ -1,24 +1,25 @@
 library(igraph)
 library(dplyr)
-
-filtered_networks <- readRDS("salinasbox/clean_data/filtered_networks.RDS")
-
-# add some info about plans to graph as attributes
 library(stringr)
-plan_deets <- readRDS("salinasbox/solarwind_project_details.RDS")
-for (i in seq_along(filtered_networks)) {
-  eis <- names(filtered_networks)[i]
-  eis_num <- str_sub(eis, -8) # get rid of "EIS_" so just number to match on
-  matches <- match(eis_num, plan_deets$EIS.Number) # get index of matches
-  if(!is.na(matches)) {
-    attr(filtered_networks[[i]], "state") <- plan_deets$State[matches]
-    attr(filtered_networks[[i]], "lead_agency") <- plan_deets$Agency[matches]
-    attr(filtered_networks[[i]], "doc_type") <- plan_deets$Document[matches]
-  }
-}
-
 library(statnet)
 library(ergm)
+
+filtered_networks <- readRDS("salinasbox/clean_data/filtered_networks.RDS")
+# add some info about plans to graph as attributes
+EIS_info <- read.csv("salinasbox/intermediate_data/project_databases/EISlist.csv")
+
+for (i in seq_along(filtered_networks)) {
+  eis <- names(filtered_networks)[i]
+  eis_num <- str_sub(eis, -8) # get rid of group prefix so just 8-digit eis number to match on
+  matches <- match(eis_num, EIS_info$EIS) # get index of matches
+  if(!is.na(matches)) {
+    attr(filtered_networks[[i]], "state") <- EIS_info$state[matches]
+    attr(filtered_networks[[i]], "lead_agency") <- EIS_info$lead_agency[matches]
+    attr(filtered_networks[[i]], "doc_type") <- EIS_info$doc_type[matches]
+    attr(filtered_networks[[i]], "year") <- EIS_info$year[matches]
+    attr(filtered_networks[[i]], "project_type") <- EIS_info$project_type[matches]
+  }
+}
 
 # get graph-level descriptives for each network... what interesting things to pull here?
 node_edge_info <- lapply(seq_along(filtered_networks), function(i) {
@@ -29,19 +30,25 @@ node_edge_info <- lapply(seq_along(filtered_networks), function(i) {
     state = attr(filtered_networks[[i]], "state"),
     doc_type = attr(filtered_networks[[i]], "doc_type"),
     lead_agency = attr(filtered_networks[[i]], "lead_agency"),
+    year = attr(filtered_networks[[i]], "year"),
+    project_type = attr(filtered_networks[[i]], "project_type"),
     num_nodes = vcount(network),
     num_edges = ecount(network),
     density = graph.density(network),
     max_degree_node = V(network)$name[degree(network)==max(degree(network))],
-    centralization_deg = centr_degree(network, mode = "total", loops = F)$centralization,
+    max_degree = max(degree(network)),
+    centraliz_between = centr_betw(network, directed = F)$centralization,
+    # how is this a measure for the entire graph 
+    centraliz_deg = centr_degree(network, mode = "total", loops = F)$centralization,
     transitivity = transitivity(network)
   )
 })
 # as data frame
 network_descriptives <- bind_rows(node_edge_info)
-
+network_descriptives$year <- str_sub(network_descriptives$EIS, 1, 4)
 
 network1nodes <- igraph::as_data_frame(filtered_networks[[1]], what = "vertices")
+igraph::as_data_frame(filtered_networks[[1]], what = "edges")
 
 vertex_attr(filtered_networks[[1]])
 # why is not resolving "state" and "colorado" for ex a problem? how does this not then like split connections and affect the calculation of network stats?
