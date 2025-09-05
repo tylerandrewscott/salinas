b021511d15650da5fd66b00459749cc505e3375b
Sara Ludwick <saraludwick@saraludwick.local>
2025-09-04 23:21:27 -0700

    get descriptives for full network and giant component

--- code/part4_networksanalysis/networkdescriptives.R
+++ code/part4_networksanalysis/networkdescriptives.R
@@ -16,11 +16,11 @@ for (i in seq_along(filtered_networks)) {
   g <- set_graph_attr(g, "eis", eis_num)
   m <- match(eis_num, as.character(EIS_info$EIS)) # get index of matches
   if(!is.na(m)) {
-    g <- set_graph_attr(g, "state",        EIS_info$state[m])
-    g <- set_graph_attr(g, "lead_agency",  EIS_info$lead_agency[m])
-    g <- set_graph_attr(g, "doc_type",     EIS_info$doc_type[m])
-    g <- set_graph_attr(g, "year",         EIS_info$year[m])
-    g <- set_graph_attr(g, "project_type", EIS_info$project_type[m])
+    g <- set_graph_attr(g, "state",        EIS_info$State[m])
+    g <- set_graph_attr(g, "lead_agency",  EIS_info$Lead.Agency[m])
+    g <- set_graph_attr(g, "doc_type",     EIS_info$Document.Type[m])
+    g <- set_graph_attr(g, "year",         EIS_info$Year[m])
+    g <- set_graph_attr(g, "project_type", EIS_info$Project.Type[m])
   }
   filtered_networks[[i]] <- g
 }
@@ -40,33 +40,74 @@ network_info <- lapply(seq_along(filtered_networks), function(i) {
 
 network_descriptives <- bind_rows(network_info)
 
-stats_by_proj <- network_descriptives %>%
-  group_by(project_type) %>%
-  summarise(
-    median_nodes = median(n_nodes),
-    IQR_nodes = IQR(n_nodes),
-    avg_nodes = mean(n_nodes),
-    sd_nodes = sd(n_nodes),
-    median_edges = median(n_edges),
-    IQR_edges = IQR(n_edges),
-    avg_edges = mean(n_edges),
-    sd_edges = sd(n_edges)
-)
-
 stats_overall <- network_descriptives %>%
   summarise(
+      median_nodes = median(n_nodes),
+      IQR_nodes = paste0(quantile(n_nodes, .25),"-",quantile(n_nodes, .75)),
+      avg_nodes = mean(n_nodes),
+      sd_nodes = sd(n_nodes),
+      median_edges = median(n_edges),
+      IQR_edges = paste0(quantile(n_edges, .25),"-",quantile(n_edges, .75)),
+      avg_edges = mean(n_edges),
+      sd_edges = sd(n_edges)) %>%
+  mutate(project_type = "All", network_type = "Full")
+
+# stats_by_proj <- network_descriptives %>%
+#   group_by(project_type) %>%
+#   summarise(
+#     median_nodes = median(n_nodes),
+#     IQR_nodes = paste0(quantile(n_nodes, .25),"-",quantile(n_nodes, .75)),
+#     avg_nodes = mean(n_nodes),
+#     sd_nodes = sd(n_nodes),
+#     median_edges = median(n_edges),
+#     IQR_edges = paste0(quantile(n_edges, .25),"-",quantile(n_edges, .75)),
+#     avg_edges = mean(n_edges),
+#     sd_edges = sd(n_edges)
+# )
+# 
+# stats_all <- bind_rows(stats_by_proj, stats_overall)
+
+##### nowfor giant component
+# extract giant component of each graph
+
+main_networks <- list()
+for (i in seq_along(filtered_networks)) {
+  g <- filtered_networks[[i]]
+  comp <- components(g) 
+  giant_c_index <- which.max(comp$csize)
+  giant_c_network <- induced.subgraph(g, which(comp$membership ==giant_c_index))  
+  main_networks[[names(filtered_networks)[i]]] <- giant_c_network  
+}
+
+giantc_info <- lapply(seq_along(main_networks), function(i) {
+  g <- main_networks[[i]]
+  attributes <- graph_attr(g)
+  list(c(attributes,
+         n_nodes = vcount(g),
+         n_edges = ecount(g),
+         density = graph.density(g)
+  )
+  )
+})
+
+giantc_descriptives <- bind_rows(giantc_info)
+stats_giantc <- giantc_descriptives %>%
+  summarise(
     median_nodes = median(n_nodes),
-    IQR_nodes = IQR(n_nodes),
+    IQR_nodes = paste0(quantile(n_nodes, .25),"-",quantile(n_nodes, .75)),
     avg_nodes = mean(n_nodes),
     sd_nodes = sd(n_nodes),
     median_edges = median(n_edges),
-    IQR_edges = IQR(n_edges),
+    IQR_edges = paste0(quantile(n_edges, .25),"-",quantile(n_edges, .75)),
     avg_edges = mean(n_edges),
-    sd_edges = sd(n_edges)
-  ) %>%
-  mutate(project_type = "all")
+    sd_edges = sd(n_edges)) %>%
+  mutate(project_type = "All", network_type = "Giant Component")
 
-stats <- bind_rows(stats_by_proj, stats_overall)
+# combine
+
+nodes_edges <- bind_rows(stats_overall, stats_giantc) %>%
+  relocate(network_type)
+
 
 
 hist(network_descriptives$n_edges)
@@ -102,16 +143,7 @@ ggplot(agency_all, aes(x = lead_agency, y= avg_centr_deg)) +
 
 
 
-# extract giant component of each graph
-main_networks <- list()
-for (i in seq_along(filtered_networks)) {
-  g <- filtered_networks[[i]]
-  comp <- components(g) 
-  giant_c_index <- which.max(comp$csize)
-  giant_c_network <- induced.subgraph(g, which(comp$membership ==giant_c_index))  
-  
-  main_networks[[names(filtered_networks)[i]]] <- giant_c_network  
-}
+
 
 
 
