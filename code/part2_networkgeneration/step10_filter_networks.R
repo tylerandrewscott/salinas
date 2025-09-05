4341292e384470be793ce0f160de9c604bf50352
Sara Ludwick <saraludwick@campus-051-192.ucdavis.edu>
2025-09-04 17:38:51 -0700

    filtered networks for ones used in final dataset

--- code/part2_networkgeneration/step10_filter_networks.R
+++ code/part2_networkgeneration/step10_filter_networks.R
@@ -1,4 +1,5 @@
 library(igraph)
+library(stringr)
 
 network_graphs <- readRDS("salinasbox/intermediate_data/network_graphs_all_entities.RDS")
 # want to filter for certain entity types
@@ -27,14 +28,22 @@ for (i in seq_along(filtered_networks)) {
     new_names[i] <- paste0(attr(filtered_networks[[i]], "group"), "_", eis_num) # make vector of new names with _group added
   } 
 }
+
 # assign new names
 names(filtered_networks) <- new_names
 filtered_networks <- filtered_networks[order(names(filtered_networks))]
 
+# manually remove networks for docs we removed after fixing preprocessing code
+network_nums <- stringr::str_extract(names(filtered_networks), "\\d{8}$")
+final_eis_nums <- readRDS("salinasbox/intermediate_data/final_eis_for_networks.RDS")
+final_nums <- as.character(final_eis_networks$EIS.Number)
+filtered_networks <- filtered_networks[network_nums %in% nums]
+
 saveRDS(filtered_networks, "salinasbox/clean_data/filtered_networks.RDS")
 
+
 ## old network graph ex
-ggraph(network_graphs[[5]], layout = "fr") +
+ggraph(network_graphs[[1]], layout = "fr") +
   geom_edge_fan(aes(alpha = weight),
                 end_cap = circle(1, "mm"),
                 color = "#000000",
@@ -44,7 +53,7 @@ ggraph(network_graphs[[5]], layout = "fr") +
   labs(title = "Example Network Plot") + theme_void()
 
 ## filtered network graph example
-ggraph(filtered_networks[[5]], layout = "fr") +
+ggraph(filtered_networks[[1]], layout = "fr") +
   geom_edge_fan(aes(alpha = weight),
                 end_cap = circle(1, "mm"),
                 color = "#000000",
@@ -52,24 +61,4 @@ ggraph(filtered_networks[[5]], layout = "fr") +
                 arrow = arrow(angle = 15, length = unit(0.07, "inches"), ends = "last", type = "closed"))+
   geom_node_point(aes(color = entity_type, size = num_appearances),  alpha = 0.6) +
   labs(title = "Example Network Plot") + theme_void()
-
-## i would really like to figure out a reasonable way to clean up nodes...eg tribe and tribes, ghg, los... i know we accept a level of tolerable noise but i don't feel greaaaaat about it... 
-
-nodes_1 <- as.data.frame(V(filtered_networks[[1]])$name)
-nodes_1$ent_type <- vertex_attr(filtered_networks[[1]], "entity_type")
-nodes_1$in_deg <- degree(filtered_networks[[1]], v = V(filtered_networks[[1]]), mode = "in")
-nodes_1$out_deg <- degree(filtered_networks[[1]], v = V(filtered_networks[[1]]), mode = "out")
-nodes_1$all_deg <- degree(filtered_networks[[1]], v = V(filtered_networks[[1]]), mode = "all")
-nodes_1 <- nodes_1[order(-nodes_1$all_deg),]
-
-nodes_3 <- as.data.frame(V(filtered_networks[[3]])$name)
-nodes_3$ent_type <- vertex_attr(filtered_networks[[3]], "entity_type")
-nodes_3$all_deg <- degree(filtered_networks[[3]], v = V(filtered_networks[[3]]), mode = "all")
-nodes_3 <- nodes_3[order(-nodes_3$all_deg),]
-
-edges_3 <- E(filtered_networks[[3]])
-edges_3
-
-
-# ex with network3
 
