a21252a25cb1dcb92723c2784ebdbbb421784133
Tyler A Scott <tscott1@gpu-4-56.farm.hpc.ucdavis.edu>
2026-01-22 14:31:42 -0800

    add .renviron

--- code/part2_networkgeneration/step7_spacyparse_and_networkextract.R
+++ code/part2_networkgeneration/step7_spacyparse_and_networkextract.R
@@ -1,130 +1,104 @@
-#Purpose: This script takes the text files we cleaned in part 1,
-#and runs spaCy on them to identify named entities and
-#token dependencies for network generation
-#then it takes those spacy outputs and groups them by EIS number
-#and runs textnet extract to generate the network files for each plan
-#those extracts are saved in intermediate_files
+# === ENVIRONMENT SETUP - MUST BE FIRST ===                                                                              
+# cuda_path <- "/cvmfs/hpc.ucdavis.edu/sw/spack/environments/core/view/generic/cuda-12.3.0"                                
+# 
+# 
 
-#Setup: requires textNet, spaCy, python, and (recommended) findpython
-#if you want to overwrite file outputs, set overwrite to T
-#(we shouldn't need to do this unless changes made to network generation process)
+overwrite <- F                                                                                                                                                                                               
+test <- T                                                                                                                                                                                                    
 
-overwrite = F
-test = T
-library(textNet)
-library(findpython)
-#spacy_parse() takes a named list where 
-#each element is a different file. 
-#let's set it up
-files <- list.files(path = "salinasbox/clean_data/pdf_to_text_clean", 
-           pattern = ".RDS", full.names = T)
+library(textNet)                                                                                                                                                                                             
+library(spacyr)                                                                                                                                                                                              
+library(stringr)                                                                                                                                                                                             
 
-texts <- vector(mode = "list", length = 
-                     length(files))
+ret_path <- "/home/tscott1/.conda/envs/textnet/bin/python"      
 
-texts <- lapply(files, function(i){
-  readRDS(i)$text
-})
+# Purpose: This script takes the text files we cleaned in part 1,                                                        
+# and runs spaCy on them to identify named entities and                                                                  
+# token dependencies for network generation                                                                              
+# then it takes those spacy outputs and groups them by EIS number                                                        
+# and runs textnet extract to generate the network files for each plan                                                   
+# those extracts are saved in intermediate_files                                                                         
+# Setup: requires textNet, spaCy, python                                                                                 
+# if you want to overwrite file outputs, set overwrite to T                                                              
 
-names(texts) <- basename(files)
+files <- list.files(path = "salinasbox/clean_data/pdf_to_text_clean",                                                    
+                    pattern = ".RDS", full.names = T)                                                                    
 
-# to resolve issues with finding python binary with find_python
-library(reticulate)
-myenv <- conda_list(conda = "auto")$python
-use_condaenv(myenv[4])
+texts <- lapply(files, function(i){                                                                                      
+  readRDS(i)$text                                                                                                        
+})                                                                                                                       
+names(texts) <- basename(files)                                                                                          
 
-ret_path <- find_python_cmd(required_modules = c('spacy', 'en_core_web_lg','en_core_web_trf'))
+parties <- c("Project", "Projects",                                                                                      
+             "Applicant", "Applicants",                                                                                  
+             "Permittee", "Permittees",                                                                                  
+             "Proponent", "Proponents",                                                                                  
+             "Band", "Bands",                                                                                            
+             "tribe", "tribes",                                                                                          
+             "Tribe", "Tribes",                                                                                          
+             "we", "We")                                                                                                 
 
-parties <- c("Project", "Projects",
-             "Applicant", "Applicants",
-             "Permittee", "Permittees",
-             "Proponent", "Proponents",
-             "Band", "Bands",
-             "tribe", "tribes",
-             "Tribe", "Tribes",
-             "we", "We")
+parse_fileloc <- paste0("salinasbox/intermediate_data/parsed_files/", basename(files))                                   
 
-#where we want to save these parsed dataframes:
-parse_fileloc <- paste0("salinasbox/intermediate_data/parsed_files/", basename(files))
+ets <- read.csv('salinasbox/dictionary_data/test_entities.csv', header = F)                                              
+ets <- ets[!grepl('\\:', ets$V2), ]                                                                                      
 
+dict_ents <- entity_specify(unique(ets$V2), case_sensitive = T, whole_word_only = T)                                     
 
-ets <- read.csv('salinasbox/dictionary_data/test_entities.csv',header = F)
-ets <- ets[!grepl('\\:',ets$V2),]
-library(stringr)
+# === RUN PARSING ===                                                                                                    
+parsed <- textNet::parse_text(ret_path,                                                                                  
+                              text_list = texts[1],                                                                      
+                              parsed_filenames = parse_fileloc,                                                          
+                              overwrite = overwrite,                                                                     
+                              test = test,                                                                               
+                              use_gpu = 'gpu',                                                                           
+                              model = "en_core_web_trf",                                                                 
+                              entity_ruler_patterns = dict_ents,                                                         
+                              overwrite_ents = T,                                                                        
+                              ruler_position = 'after',                                                                  
+                              custom_entities = list(PARTIES = parties))                                                 
 
-dict_ents <- entity_specify(unique(ets$V2),case_sensitive = T,whole_word_only = T)
+names(parsed) <- names(texts)                                                                                            
+saveRDS(object = parsed, file = "salinasbox/intermediate_data/all_parsed.RDS")                                           
 
-parsed <- textNet::parse_text(ret_path,
-                              text_list = texts[1:2],
-                              parsed_filenames = parse_fileloc,
-                              overwrite = overwrite,
-                              ##### test = T forces run
-                              ##### otherwise, if file already exists, will load
-                              test = test,
-                              #### will use gpu on FARM ###
-                              use_gpu = 'auto',
-                              ### NEW THING I CHANGED MODEL ####
-                              model = "en_core_web_trf",
-                              ### add entityrulers ###
-                              entity_ruler_patterns = dict_ents,
-                              ### override if NER gaps a entityruler object ###
-                              overwrite_ents = T,
-                              ### entityruler has final say ### 
-                              ruler_position = 'after'
-                              custom_entities = list(PARTIES = parties))
+# === GROUP BY EIS NUMBER ===                                                                                            
+projects <- vector(mode = "list", length = length(unique(substr(basename(files), 1, 8))))                                
+names(projects) <- unique(substr(basename(files), 1, 8))                                                                 
 
-names(parsed) <- names(texts)
-saveRDS(object = parsed, file = "salinasbox/intermediate_data/all_parsed.RDS")
+filenum <- 1                                                                                                             
+for(i in 1:length(projects)){                                                                                            
+  projects[[i]] <- parsed[[filenum]]                                                                                     
+  filenum <- filenum + 1                                                                                                 
+  while(filenum <= length(parsed) & substr(names(parsed)[filenum], 1, 8) == names(projects)[i]){                         
+    projects[[i]] <- rbind(projects[[i]], parsed[[filenum]])                                                             
+    filenum <- filenum + 1                                                                                               
+  }                                                                                                                      
+}                                                                                                                        
 
-#put all parts of same eis number together
+# === EXTRACT NETWORKS ===                                                                                               
+extracts <- vector(mode = "list", length = length(projects))                                                             
 
-projects <- vector(mode = "list", length = 
-                     length(unique(substr(basename(files), 1, 8))))
+keptentities <- c("PERSON",                                                                                              
+                  "NORP",                                                                                                
+                  "FAC",                                                                                                 
+                  "ORG", "GPE",                                                                                          
+                  "LOC", "PRODUCT",                                                                                      
+                  "EVENT", "WORK_OF_ART",                                                                                
+                  "LAW", "LANGUAGE",                                                                                     
+                  "PARTIES")                                                                                             
 
-names(projects) <- unique(substr(basename(files), 1, 8))
+for(m in 1:length(projects)){                                                                                            
+  extract_file <- paste0("salinasbox/intermediate_data/raw_extracted_networks/extract_", names(projects)[m], ".RDS")     
+  if(overwrite == T | !file.exists(extract_file)){                                                                       
+    extracts[[m]] <- textnet_extract(projects[[m]],                                                                      
+                                     cl = 4,                                                                             
+                                     keep_entities = keptentities,                                                       
+                                     return_to_memory = T,                                                               
+                                     keep_incomplete_edges = T,                                                          
+                                     file = extract_file)                                                                
+  } else {                                                                                                               
+    print(paste0("file ", extract_file, " already exists."))                                                             
+  }                                                                                                                      
+}                                                                                                                        
 
-filenum = 1
-for(i in 1:length(projects)){
-  projects[[i]] <- parsed[[filenum]]
-  filenum = filenum + 1
-  while(filenum <= length(parsed) & substr(names(parsed)[filenum], 1, 8) == names(projects)[i]){
-    projects[[i]] <- rbind(projects[[i]], parsed[[filenum]])
-    filenum = filenum + 1
-  }
-}
-
-extracts <- vector(mode = "list", length = length(projects))
-#better to be inclusive with entity types and remove later
-#see link below, page 21 for definitions
-#https://catalog.ldc.upenn.edu/docs/LDC2013T19/OntoNotes-Release-5.0.pdf
-
-#"EVENT" doesn't have much in it but we will preserve just in case
-#"LANGUAGE" doesn't have much in it but sometimes "Latino" (??)
-#did not keep "MONEY" because it appeared unreliable (sometimes was kJ, etc.)
-keptentities <- c("PERSON", 
-              "NORP", 
-              "FAC",
-              "ORG", "GPE", 
-              "LOC", "PRODUCT", 
-              "EVENT", "WORK_OF_ART",
-              "LAW", "LANGUAGE",
-              "PARTIES")
-for(m in 1:length(projects)){
-  if(overwrite ==T | !file.exists(paste0("salinasbox/intermediate_data/raw_extracted_networks/extract_", names(projects)[m],".RDS"))){
-    extracts[[m]] <- textnet_extract(projects[[m]], 
-                                     cl = 4,
-                                     keep_entities = keptentities,
-                                     return_to_memory = T,
-                                     keep_incomplete_edges = T,
-                                     file = paste0("salinasbox/intermediate_data/raw_extracted_networks/extract_", names(projects)[m],".RDS")
-    )
-  }else{
-    print(paste0("file ", paste0("salinasbox/intermediate_data/raw_extracted_networks/extract_", names(projects)[m],".RDS"),
-                 " already exists."))
-  }
-  
-}
 saveRDS(object = extracts, file = "salinasbox/intermediate_data/raw_extracts.RDS")
-
-
-
